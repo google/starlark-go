6f720227f76fd6b7c5f2c8278c1cdbe78bd862e2
Alan Donovan <adonovan@google.com>
2017-10-10 15:51:22 -0400

    cmd/skylark: REPL: support load, and other improvements
    1. Loading: the REPL now implements module loading, following the
       sequential implementation in example_test.go. Repeated loads of the
       same module have no effect.
    
    2. Parsing of load statements: the REPL's two-pass approach to parsing
       the input, first trying as an expression, second as a file, led it
       to mishandle load(...) since it only becomes a LoadStmt when it
       appears in a statement context. (This is a sad part of the spec.)
       Now the REPL always treats a load(...) call as a file.
    
    3. Multi-line expressions: when the REPL cannot parse a line as an
       expression, it reads a multi-line block terminated by a blank line.
       It used to parse such a block as a file, for side effects only.
       That meant it would not show the value of an expression spread across
       multiple lines. Now it tries again to parse the multi-line block as
       an expression before trying as a file.
    
    4. No freeze: the REPL no longer freezes the values of global
       variables, so you can say:
           x = 1
           ...
           x = 2
       split across two different chunks of input.
    
    Change-Id: I3894defbc404a0b1d2b2d9e637b2b98c1b3f9299

--- cmd/skylark/skylark.go
+++ cmd/skylark/skylark.go
@@ -3,14 +3,39 @@
 // license that can be found in the LICENSE file.
 
 // The skylark command interprets a Skylark file.
+//
 // With no arguments, it starts a read-eval-print loop (REPL).
+// If an input line can be parsed as an expression,
+// the REPL parses and evaluates it and prints its result.
+// Otherwise the REPL reads lines until a blank line,
+// then tries again to parse the multi-line input as an
+// expression. If the input still cannot be parsed as an expression,
+// the REPL parses and executes it as a file (a list of statements),
+// for side effects.
 package main
+
+// TODO(adonovan):
+//
+// - Distinguish expressions from statements more precisely.
+//   Otherwise e.g. 1 is parsed as an expression but
+//   1000000000000000000000000000 is parsed as a file
+//   because the scanner fails to convert it to an int64.
+//   The spec should clarify limits on numeric literals.
+//
+// - Unparenthesized tuples are not parsed as a single expression:
+//     >>> (1, 2)
+//     (1, 2)
+//     >>> 1, 2
+//     ...
+//     >>>
+//   This is not necessarily a bug.
 
 import (
 	"bufio"
 	"bytes"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"runtime/pprof"
@@ -20,7 +45,6 @@ import (
 	"github.com/google/skylark"
 	"github.com/google/skylark/resolve"
 	"github.com/google/skylark/syntax"
-	"io"
 )
 
 // flags
@@ -65,7 +89,7 @@ func main() {
 }
 
 func execfile(filename string) {
-	thread := new(skylark.Thread)
+	thread := &skylark.Thread{Load: load}
 	globals := make(skylark.StringDict)
 	if err := skylark.ExecFile(thread, filename, nil, globals); err != nil {
 		printError(err)
@@ -88,7 +112,7 @@ func execfile(filename string) {
 }
 
 func repl() {
-	thread := new(skylark.Thread)
+	thread := &skylark.Thread{Load: load}
 	globals := make(skylark.StringDict)
 
 	sc := bufio.NewScanner(os.Stdin)
@@ -105,7 +129,7 @@ outer:
 
 		// If the line contains a well-formed
 		// expression, evaluate it.
-		if _, err := syntax.ParseExpr("<stdin>", line); err == nil {
+		if expr, err := syntax.ParseExpr("<stdin>", line); err == nil && !isLoad(expr) {
 			if v, err := skylark.Eval(thread, "<stdin>", line, globals); err != nil {
 				printError(err)
 			} else if v != skylark.None {
@@ -129,11 +153,82 @@ outer:
 			}
 			fmt.Fprintln(&buf, line)
 		}
-		if err := skylark.ExecFile(thread, "<stdin>", &buf, globals); err != nil {
+		text := buf.Bytes()
+
+		// Try parsing it once more as an expression,
+		// such as a call spread over several lines:
+		//   f(
+		//     1,
+		//     2
+		//   )
+		if expr, err := syntax.ParseExpr("<stdin>", text); err == nil && !isLoad(expr) {
+			if v, err := skylark.Eval(thread, "<stdin>", text, globals); err != nil {
+				printError(err)
+			} else if v != skylark.None {
+				fmt.Println(v)
+			}
+			continue
+		}
+
+		// Execute it as a file.
+		if err := execFileNoFreeze(thread, text, globals); err != nil {
 			printError(err)
 		}
 	}
 	fmt.Println()
+}
+
+// execFileNoFreeze is skylark.ExecFile without globals.Freeze().
+func execFileNoFreeze(thread *skylark.Thread, src interface{}, globals skylark.StringDict) error {
+	// parse
+	f, err := syntax.Parse("<stdin>", src)
+	if err != nil {
+		return err
+	}
+
+	// resolve
+	isPredeclaredGlobal := func(name string) bool { return globals[name] != nil }
+	isBuiltin := func(name string) bool { return skylark.Universe[name] != nil }
+	if err := resolve.File(f, isPredeclaredGlobal, isBuiltin); err != nil {
+		return err
+
+	}
+
+	// execute
+	fr := thread.Push(globals, len(f.Locals))
+	defer thread.Pop()
+	return fr.ExecStmts(f.Stmts)
+}
+
+type entry struct {
+	globals skylark.StringDict
+	err     error
+}
+
+var cache = make(map[string]*entry)
+
+// load is a simple sequential implementation of module loading.
+func load(thread *skylark.Thread, module string) (skylark.StringDict, error) {
+	e, ok := cache[module]
+	if e == nil {
+		if ok {
+			// request for package whose loading is in progress
+			return nil, fmt.Errorf("cycle in load graph")
+		}
+
+		// Add a placeholder to indicate "load in progress".
+		cache[module] = nil
+
+		// Load it.
+		thread := &skylark.Thread{Load: load}
+		globals := make(skylark.StringDict)
+		err := skylark.ExecFile(thread, module, nil, globals)
+		e = &entry{globals, err}
+
+		// Update the cache.
+		cache[module] = e
+	}
+	return e.globals, e.err
 }
 
 func printError(err error) {
@@ -142,4 +237,17 @@ func printError(err error) {
 	} else {
 		fmt.Fprintln(os.Stderr, err)
 	}
+}
+
+// isLoad reports whether e is a load(...) function call.
+// If so, we must parse it again as a file, not an expression,
+// so that it is is converted to a load statement.
+// ("load" should really be a reserved word.)
+func isLoad(e syntax.Expr) bool {
+	if call, ok := e.(*syntax.CallExpr); ok {
+		if id, ok := call.Fn.(*syntax.Ident); ok && id.Name == "load" {
+			return true
+		}
+	}
+	return false
 }
