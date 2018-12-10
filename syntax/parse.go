google/starlark-go

commit 6be536f2cdd8f0f7c667a7e81c310f3eba95948a

[parser] Unify * and ** parsing. (#57)

syntax/parse.go
@@ -406,27 +406,14 @@ func (p *parser) parseParams() []Expr {
 			break
 		}
 
-		// *args
-		if p.tok == STAR {
+		// *args or **kwargs
+		if p.tok == STAR || p.tok == STARSTAR {
 			stars = true
 			pos := p.nextToken()
 			id := p.parseIdent()
 			params = append(params, &UnaryExpr{
 				OpPos: pos,
-				Op:    STAR,
-				X:     id,
-			})
-			continue
-		}
-
-		// **kwargs
-		if p.tok == STARSTAR {
-			stars = true
-			pos := p.nextToken()
-			id := p.parseIdent()
-			params = append(params, &UnaryExpr{
-				OpPos: pos,
-				Op:    STARSTAR,
+				Op:    p.tok,
 				X:     id,
 			})
 			continue

commit d9159adc4faa860d71210f41101bf99c968385c1

syntax: fix bug in parser
(introduced in 6be536f)

Also factor similar *args/*kwargs parsing in the same way.

Change-Id: Ia1f2ef88b85eb920ce397e61f3aa9f5b16387456

syntax/parse.go
@@ -409,11 +409,12 @@ func (p *parser) parseParams() []Expr {
 		// *args or **kwargs
 		if p.tok == STAR || p.tok == STARSTAR {
 			stars = true
+			op := p.tok
 			pos := p.nextToken()
 			id := p.parseIdent()
 			params = append(params, &UnaryExpr{
 				OpPos: pos,
-				Op:    p.tok,
+				Op:    op,
 				X:     id,
 			})
 			continue
@@ -703,27 +704,15 @@ func (p *parser) parseArgs() []Expr {
 			break
 		}
 
-		// *args
-		if p.tok == STAR {
+		// *args or **kwargs
+		if p.tok == STAR || p.tok == STARSTAR {
 			stars = true
+			op := p.tok
 			pos := p.nextToken()
 			x := p.parseTest()
 			args = append(args, &UnaryExpr{
 				OpPos: pos,
-				Op:    STAR,
-				X:     x,
-			})
-			continue
-		}
-
-		// **kwargs
-		if p.tok == STARSTAR {
-			stars = true
-			pos := p.nextToken()
-			x := p.parseTest()
-			args = append(args, &UnaryExpr{
-				OpPos: pos,
-				Op:    STARSTAR,
+				Op:    op,
 				X:     x,
 			})
 			continue