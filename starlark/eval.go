bac70fed54f64cf24945ee79511178a4b0832bc3
Alan A. Donovan <adonovan@google.com>
2019-07-02 15:26:26 -0400

    starlark: refactor: move Unpack functions into their own file.
    Change-Id: Ic40cba85aae1821abc7293045da15f0c8106b8ba

--- starlark/eval.go
+++ starlark/eval.go
@@ -1304,49 +1304,6 @@ func findParam(params []compile.Binding, name string) int {
 	return -1
 }
 
-type intset struct {
-	small uint64       // bitset, used if n < 64
-	large map[int]bool //    set, used if n >= 64
-}
-
-func (is *intset) init(n int) {
-	if n >= 64 {
-		is.large = make(map[int]bool)
-	}
-}
-
-func (is *intset) set(i int) (prev bool) {
-	if is.large == nil {
-		prev = is.small&(1<<uint(i)) != 0
-		is.small |= 1 << uint(i)
-	} else {
-		prev = is.large[i]
-		is.large[i] = true
-	}
-	return
-}
-
-func (is *intset) get(i int) bool {
-	if is.large == nil {
-		return is.small&(1<<uint(i)) != 0
-	}
-	return is.large[i]
-}
-
-func (is *intset) len() int {
-	if is.large == nil {
-		// Suboptimal, but used only for error reporting.
-		len := 0
-		for i := 0; i < 64; i++ {
-			if is.small&(1<<uint(i)) != 0 {
-				len++
-			}
-		}
-		return len
-	}
-	return len(is.large)
-}
-
 // https://github.com/google/starlark-go/blob/master/doc/spec.md#string-interpolation
 func interpolate(format string, x Value) (Value, error) {
 	buf := new(strings.Builder)
