e55f603d8c797b182566012861c5784481d00df4
alandonovan <adonovan@google.com>
2020-11-18 13:34:35 -0500

    starlark: use portable syscall wrapper for mmap (#321)
    Also, add test to ensure that new dependencies (such as golang.org/x/sys/unix)
    are not added casually.
    
    Fixes #320

--- starlark/int_posix64.go
+++ starlark/int_posix64.go
@@ -22,9 +22,9 @@ import (
 	"log"
 	"math"
 	"math/big"
-	"runtime"
-	"syscall"
 	"unsafe"
+
+	"golang.org/x/sys/unix"
 )
 
 // intImpl represents a union of (int32, *big.Int) in a single pointer,
@@ -59,15 +59,7 @@ func makeBigInt(x *big.Int) Int { return Int{intImpl(x)} }
 var smallints = reserveAddresses(1 << 32)
 
 func reserveAddresses(len int) uintptr {
-	// Use syscall to avoid golang.org/x/sys/unix dependency.
-	MAP_ANON := 0x1000 // darwin (and all BSDs)
-	switch runtime.GOOS {
-	case "linux", "android":
-		MAP_ANON = 0x20
-	case "solaris":
-		MAP_ANON = 0x100
-	}
-	b, err := syscall.Mmap(-1, 0, len, syscall.PROT_READ, syscall.MAP_PRIVATE|MAP_ANON)
+	b, err := unix.Mmap(-1, 0, len, unix.PROT_READ, unix.MAP_PRIVATE|unix.MAP_ANON)
 	if err != nil {
 		log.Fatalf("mmap: %v", err)
 	}
