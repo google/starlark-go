00e681c55729891c983863a25161239b91320f8a
essobedo <nicolas_filotto@ultimatesoftware.com>
2021-03-08 10:41:49 +0100

    math: Applied remarks part 2

--- lib/math/math.go
+++ lib/math/math.go
@@ -6,52 +6,85 @@
 package math // import "go.starlark.net/lib/math"
 
 import (
+	"fmt"
 	"math"
 
 	"go.starlark.net/starlark"
 	"go.starlark.net/starlarkstruct"
 )
 
-const (
-	tau    = math.Pi * 2
-	oneRad = tau / 360
-)
-
-var (
-	toDeg = func(x float64) float64 { return x / oneRad }
-	toRad = func(x float64) float64 { return x * oneRad }
-)
-
 // Module math is a Starlark module of math-related functions and constants.
+// It defines the following functions:
+//
+//     abs(x) - Returns the absolute value of x.
+//     ceil(x) - Returns the ceiling of x, the smallest integer greater than or equal to x.
+//     floor(x) - Returns the floor of x, the largest integer less than or equal to x.
+//     round(x) - Returns the nearest integer, rounding half away from zero.
+//
+//     exp(x) - Returns e raised to the power x, where e = 2.718281… is the base of natural logarithms.
+//     sqrt(x) - Returns the square root of x.
+//
+//     acos(x) - Returns the arc cosine of x, in radians.
+//     asin(x) - Returns the arc sine of x, in radians.
+//     atan(x) - Returns the arc tangent of x, in radians.
+//     atan2(y, x) - Returns atan(y / x), in radians.
+//                   The result is between -pi and pi.
+//                   The vector in the plane from the origin to point (x, y) makes this angle with the positive X axis.
+//                   The point of atan2() is that the signs of both inputs are known to it, so it can compute the correct
+//                   quadrant for the angle.
+//                   For example, atan(1) and atan2(1, 1) are both pi/4, but atan2(-1, -1) is -3*pi/4.
+//     cos(x) - Returns the cosine of x, in radians.
+//     hypot(x, y) - Returns the Euclidean norm, sqrt(x*x + y*y). This is the length of the vector from the origin to point (x, y).
+//     sin(x) - Returns the sine of x, in radians.
+//     tan(x) - Returns the tangent of x, in radians.
+//
+//     degrees(x) - Converts angle x from radians to degrees.
+//     radians(x) - Converts angle x from degrees to radians.
+//
+//     acosh(x) - Returns the inverse hyperbolic cosine of x.
+//     asinh(x) - Returns the inverse hyperbolic sine of x.
+//     atanh(x) - Returns the inverse hyperbolic tangent of x.
+//     cosh(x) - Returns the hyperbolic cosine of x.
+//     sinh(x) - Returns the hyperbolic sine of x.
+//     tanh(x) - Returns the hyperbolic tangent of x.
+//
+// All function accept both int and float values as arguments.
+//
+// It defines the following constants:
+//
+//     e - The number e, also known as Euler's number, is a mathematical constant approximately equal to 2.71828.
+//     phi - The number phi, also known as the Golden Ratio, is a mathematical constant approximately equal to 1.61803.
+//     pi - The number pi, also known as Archimedes' constant, is a mathematical constant approximately equal to 3.14159.
+//
 var Module = &starlarkstruct.Module{
 	Name: "math",
 	Members: starlark.StringDict{
-		"abs":   starlark.NewBuiltin("abs", abs),
-		"ceil":  starlark.NewBuiltin("ceil", ceil),
-		"floor": starlark.NewBuiltin("floor", floor),
-		"round": starlark.NewBuiltin("round", round),
+		"abs":   newUnaryBuiltin("abs", math.Abs),
+		"ceil":  newUnaryBuiltin("ceil", math.Ceil),
+		"floor": newUnaryBuiltin("floor", math.Floor),
+		"round": newUnaryBuiltin("round", math.Round),
 
-		"exp":  starlark.NewBuiltin("exp", exp),
-		"sqrt": starlark.NewBuiltin("sqrt", sqrt),
+		"exp":  newUnaryBuiltin("exp", math.Exp),
+		"sqrt": newUnaryBuiltin("sqrt", math.Sqrt),
 
-		"acos":  starlark.NewBuiltin("acos", acos),
-		"asin":  starlark.NewBuiltin("asin", asin),
-		"atan":  starlark.NewBuiltin("atan", atan),
-		"atan2": starlark.NewBuiltin("atan2", atan2),
-		"cos":   starlark.NewBuiltin("cos", cos),
-		"hypot": starlark.NewBuiltin("hypot", hypot),
-		"sin":   starlark.NewBuiltin("sin", sin),
-		"tan":   starlark.NewBuiltin("tan", tan),
+		"acos":  newUnaryBuiltin("acos", math.Acos),
+		"asin":  newUnaryBuiltin("asin", math.Asin),
+		"atan":  newUnaryBuiltin("atan", math.Atan),
+		"atan2": newBinaryFunction("atan2", math.Atan2),
+		"cos":   newUnaryBuiltin("cos", math.Cos),
+		"hypot": newBinaryFunction("hypot", math.Hypot),
+		"sin":   newUnaryBuiltin("sin", math.Sin),
+		"tan":   newUnaryBuiltin("tan", math.Tan),
 
-		"degrees": starlark.NewBuiltin("degrees", degrees),
-		"radians": starlark.NewBuiltin("radians", radians),
+		"degrees": newUnaryBuiltin("degrees", toDeg),
+		"radians": newUnaryBuiltin("radians", toRad),
 
-		"acosh": starlark.NewBuiltin("acosh", acosh),
-		"asinh": starlark.NewBuiltin("asinh", asinh),
-		"atanh": starlark.NewBuiltin("atanh", atanh),
-		"cosh":  starlark.NewBuiltin("cosh", cosh),
-		"sinh":  starlark.NewBuiltin("sinh", sinh),
-		"tanh":  starlark.NewBuiltin("tanh", tanh),
+		"acosh": newUnaryBuiltin("acosh", math.Acosh),
+		"asinh": newUnaryBuiltin("asinh", math.Asinh),
+		"atanh": newUnaryBuiltin("atanh", math.Atanh),
+		"cosh":  newUnaryBuiltin("cosh", math.Cosh),
+		"sinh":  newUnaryBuiltin("sinh", math.Sinh),
+		"tanh":  newUnaryBuiltin("tanh", math.Tanh),
 
 		"e":   starlark.Float(math.E),
 		"phi": starlark.Float(math.Phi),
@@ -59,140 +92,51 @@ var Module = &starlarkstruct.Module{
 	},
 }
 
-// oneArgFunc unpacks a starlark function call with one argument, calls a passed in function taking one float64 as argument
-// and returns the result as a starlark value.
-func oneArgFunc(name string, args starlark.Tuple, kwargs []starlark.Tuple, fn func(float64) float64) (starlark.Value, error) {
-	var x float64
-	if err := starlark.UnpackPositionalArgs(name, args, kwargs, 1, &x); err != nil {
-		var i int64
-		if starlark.UnpackPositionalArgs(name, args, kwargs, 1, &i) != nil {
+// The funcParam type of all the math functions.
+type funcParam float64
+
+// Unpack is a custom argument unpacker
+func (p *funcParam) Unpack(v starlark.Value) error {
+	switch x := v.(type) {
+	case starlark.Int:
+		*p = funcParam(x.Float())
+		return nil
+	case starlark.Float:
+		*p = funcParam(x)
+		return nil
+	}
+
+	return fmt.Errorf("got %s, want a float or an int", v.Type())
+}
+
+// newUnaryBuiltin wraps a unary floating-point Go function
+// as a Starlark built-in that accepts int or float arguments.
+func newUnaryBuiltin(name string, fn func(float64) float64) *starlark.Builtin {
+	return starlark.NewBuiltin(name, func(thread *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+		var x funcParam
+		if err := starlark.UnpackPositionalArgs(name, args, kwargs, 1, &x); err != nil {
 			return nil, err
 		}
-		x = float64(i)
-	}
-	return starlark.Float(fn(x)), nil
+		return starlark.Float(fn(float64(x))), nil
+	})
 }
 
-// twoArgFunc unpacks a starlark function call with two arguments, calls a passed in function taking two float64 as argument
-// and returns the result as a starlark value.
-func twoArgFunc(name string, args starlark.Tuple, kwargs []starlark.Tuple, fn func(float64, float64) float64) (starlark.Value, error) {
-	var x, y float64
-	if err := starlark.UnpackPositionalArgs(name, args, kwargs, 2, &x, &y); err != nil {
-		return nil, err
-	}
-	return starlark.Float(fn(x, y)), nil
+// newBinaryFunction wraps a binary floating-point Go function
+// as a Starlark built-in that accepts int or float arguments.
+func newBinaryFunction(name string, fn func(float64, float64) float64) *starlark.Builtin {
+	return starlark.NewBuiltin(name, func(thread *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+		var x, y funcParam
+		if err := starlark.UnpackPositionalArgs(name, args, kwargs, 2, &x, &y); err != nil {
+			return nil, err
+		}
+		return starlark.Float(fn(float64(x), float64(y))), nil
+	})
 }
 
-// floor(x) - Return the floor of x, the largest integer less than or equal to x.
-func floor(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("floor", args, kwargs, math.Floor)
+func toDeg(x float64) float64 {
+	return 360 * x / (2 * math.Pi)
 }
 
-// ceil(x) - Return the ceiling of x, the smallest integer greater than or equal to x.
-func ceil(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("ceil", args, kwargs, math.Ceil)
-}
-
-// abs(x) - Return the absolute value of x.
-func abs(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("abs", args, kwargs, math.Abs)
-}
-
-// round(x) - Round returns the nearest integer, rounding half away from zero.
-func round(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("round", args, kwargs, math.Round)
-}
-
-// exp(x) - Return e raised to the power x, where e = 2.718281… is the base of natural logarithms.
-func exp(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("exp", args, kwargs, math.Exp)
-}
-
-// sqrt(x) - Return the square root of x.
-func sqrt(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("sqrt", args, kwargs, math.Sqrt)
-}
-
-// acos(x) - Return the arc cosine of x, in radians.
-func acos(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("acos", args, kwargs, math.Acos)
-}
-
-// asin(x) - Return the arc sine of x, in radians.
-func asin(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("asin", args, kwargs, math.Asin)
-}
-
-// atan(x) - Return the arc tangent of x, in radians.
-func atan(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("atan", args, kwargs, math.Atan)
-}
-
-// atan2(y, x) - Return atan(y / x), in radians.
-// The result is between -pi and pi.
-// The vector in the plane from the origin to point (x, y) makes this angle with the positive X axis.
-// The point of atan2() is that the signs of both inputs are known to it, so it can compute the correct quadrant for the angle.
-// For example, atan(1) and atan2(1, 1) are both pi/4, but atan2(-1, -1) is -3*pi/4.
-func atan2(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return twoArgFunc("atan2", args, kwargs, math.Atan2)
-}
-
-// cos(x) - Return the cosine of x, in radians.
-func cos(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("cos", args, kwargs, math.Cos)
-}
-
-// hypot(x, y) - Return the Euclidean norm, sqrt(x*x + y*y). This is the length of the vector from the origin to point (x, y).
-func hypot(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return twoArgFunc("hypot", args, kwargs, math.Hypot)
-}
-
-// sin(x) - Return the sine of x, in radians.
-func sin(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("sin", args, kwargs, math.Sin)
-}
-
-// tan(x) - Return the tangent of x, in radians.
-func tan(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("tan", args, kwargs, math.Tan)
-}
-
-// degrees(x) - Convert angle x from radians to degrees.
-func degrees(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("degrees", args, kwargs, toDeg)
-}
-
-// radians(x) - Convert angle x from degrees to radians.
-func radians(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("radians", args, kwargs, toRad)
-}
-
-// acosh(x) - Return the inverse hyperbolic cosine of x.
-func acosh(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("acosh", args, kwargs, math.Acosh)
-}
-
-// asinh(x) - Return the inverse hyperbolic sine of x.
-func asinh(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("asinh", args, kwargs, math.Asinh)
-}
-
-// atanh(x) - Return the inverse hyperbolic tangent of x.
-func atanh(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("atanh", args, kwargs, math.Atanh)
-}
-
-// cosh(x) - Return the hyperbolic cosine of x.
-func cosh(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("cosh", args, kwargs, math.Cosh)
-}
-
-// sinh(x) - Return the hyperbolic sine of x.
-func sinh(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("sinh", args, kwargs, math.Sinh)
-}
-
-// tanh(x) - Return the hyperbolic tangent of x.
-func tanh(thread *starlark.Thread, _ *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-	return oneArgFunc("tanh", args, kwargs, math.Tanh)
+func toRad(x float64) float64 {
+	return 2 * math.Pi * x / 360
 }
