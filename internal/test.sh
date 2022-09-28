5fccb4daaf6d81d6de296eb308ddcd8a11d36b4b
Alan Donovan <adonovan@google.com>
2022-09-28 02:38:52 -0400

    Tidy go.mod file (#424)
    Also improve failure message of tidiness test.

--- internal/test.sh
+++ internal/test.sh
@@ -6,13 +6,13 @@
 
 set -eu
 
-# Confirm that go.mod and go.sum are tidy
+# Confirm that go.mod and go.sum are tidy.
 cp go.mod go.mod.orig
 cp go.sum go.sum.orig
 go mod tidy
 # Use -w to ignore differences in OS newlines.
-diff -w  go.mod.orig go.mod
-diff -w go.sum.orig go.sum
+diff -w go.mod.orig go.mod || { echo "go.mod is not tidy"; exit 1; }
+diff -w go.sum.orig go.sum || { echo "go.sum is not tidy"; exit 1; }
 rm go.mod.orig go.sum.orig
 
 # Run tests
