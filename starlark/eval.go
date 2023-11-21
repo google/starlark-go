90ade8b19d09805d1b91a9687198869add6dfaa1
kcza <ed.jones@canonical.com>
2023-11-21 15:53:37 +0000

    Remove 'too many arguments' error when interpolating over mappings (#520)
    * Fix inconsistent interpolation with empty mappings and tuples
    
    * Clarify spec when no conversions are present
    
    * Rename `any` function
    
    * Only check for `Mapping` in the uncommon case

--- starlark/eval.go
+++ starlark/eval.go
@@ -1659,9 +1659,14 @@ func interpolate(format string, x Value) (Value, error) {
 		index++
 	}
 
-	if index < nargs {
+	if index < nargs && !is[Mapping](x) {
 		return nil, fmt.Errorf("too many arguments for format string")
 	}
 
 	return String(buf.String()), nil
+}
+
+func is[T any](x any) bool {
+	_, ok := x.(T)
+	return ok
 }
